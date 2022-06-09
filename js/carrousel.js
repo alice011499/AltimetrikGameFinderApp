b23ad35bd4c8d35cee31a9f19010d1e3eee699b2
Alice Lettiere <allettiere@gmail.com>
2022-06-09 02:39:02 -0300

    [2022/06/09] Still working on the login. I've no computer so the workflow is quite difficult to follow.

---
+++ js/carrousel.js
@@ -0,0 +1,6 @@
+// const buttonleft = document.querySelector(".carrousel_btn--left");
+// console.log(buttonleft);
+// buttonleft.addEventListener("click", function() {
+//     console.log("clicked!");
+//     buttonleft.style = "background-color: green; border-radius: 0px; margin: 0 200px";
+// })
